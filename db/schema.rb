eefbd8be14157f40285b11b881f53f7392a3f822
Ben Hoskings <ben@hoskings.net>
2010-05-09 10:44:39 -0400

    before adding posts and categories

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100508174105) do
+ActiveRecord::Schema.define(:version => 20100508214355) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
