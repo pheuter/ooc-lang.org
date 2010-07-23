d698c00ebd9ddfa200df579215f0f1e66f042d7b
Mark Fayngersh <phunny.phacts@gmail.com>
2010-07-23 14:00:40 -0400

    alpaca revisited

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
+  require 'open-uri'
   def index
+    begin
+      @linux = "<p style='color:green;'>"+(Hpricot(open('http://server.markfayngersh.com:4567'))/"ul[@class='posts']").search("li")[1].inner_html.squeeze(" ").strip.gsub(/\n/,'').match(/(.+)\(worked\)/)[1]+"</p>"
+    rescue NoMethodError => e
+      @linux = "<p style='color:red;'>"+(Hpricot(open('http://server.markfayngersh.com:4567'))/"ul[@class='posts']").search("li")[1].inner_html.squeeze(" ").strip.gsub(/\n/,'').match(/(.+)\(failed\)/)[1]+"</p>"
+    end
   end
-
 end
