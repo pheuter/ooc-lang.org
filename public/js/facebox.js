f7a2d8c9bb5e73497fcb84a859dcba4d7e3b34bd
Mark Fayngersh <phunny.phacts@gmail.com>
2010-09-21 16:38:33 -0400

    image links fix

--- public/js/facebox.js
+++ public/js/facebox.js
@@ -79,8 +79,8 @@
     settings: {
       opacity      : 0,
       overlay      : true,
-      loadingImage : '/facebox/loading.gif',
-      closeImage   : '/facebox/closelabel.gif',
+      loadingImage : '/img/loading.gif',
+      closeImage   : '/img/closelabel.gif',
       imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
       faceboxHtml  : '\
     <div id="facebox" style="display:none;"> \
@@ -97,7 +97,7 @@
                 </div> \
                 <div class="footer"> \
                   <a href="#" class="close"> \
-                    <img src="/facebox/closelabel.gif" title="close" class="close_image" /> \
+                    <img src="/img/closelabel.gif" title="close" class="close_image" /> \
                   </a> \
                 </div> \
               </td> \
