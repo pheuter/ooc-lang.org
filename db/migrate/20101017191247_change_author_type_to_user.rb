af932650b115860351302a38d4de11a93faf34bb
Mark Fayngersh <phunny.phacts@gmail.com>
2010-10-17 15:14:45 -0400

    change type of author to user

---
+++ db/migrate/20101017191247_change_author_type_to_user.rb
@@ -0,0 +1,10 @@
+class ChangeAuthorTypeToUser < ActiveRecord::Migration
+  def self.up
+    change_table :posts do |t|
+      t.references  :author
+    end
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101017174651) do
+ActiveRecord::Schema.define(:version => 20101017191247) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20101017174651) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "markdown"
+    t.integer  "author_id"
   end
 
   create_table "slogans", :force => true do |t|
