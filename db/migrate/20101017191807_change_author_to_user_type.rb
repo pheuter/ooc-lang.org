5f1b8d8e766b15e6081ccde877af69890417c273
Mark Fayngersh <phunny.phacts@gmail.com>
2010-10-17 15:30:25 -0400

    change to post table attempt 3

--- db/migrate/20101017191807_change_author_to_user_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeAuthorToUserType < ActiveRecord::Migration
-  def self.up
-    add_column :posts, :author, :reference
-  end
-
-  def self.down
-    remove_column :posts, :author
-  end
-end
