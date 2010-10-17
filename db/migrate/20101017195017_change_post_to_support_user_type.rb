bc0c424dae5ad43adf11bb5fc51ca2eb834bf5f7
Mark Fayngersh <phunny.phacts@gmail.com>
2010-10-17 15:50:36 -0400

    trying to update author column

--- db/migrate/20101017195017_change_post_to_support_user_type.rb
+++ db/migrate/20101017195017_change_post_to_support_user_type.rb
@@ -1,11 +1,11 @@
 class ChangePostToSupportUserType < ActiveRecord::Migration
   def self.up
     change_table :posts do |t|
-      t.remove :author
       t.references :author
     end
   end
 
   def self.down
+    remove_column :posts, :author
   end
 end
